f2a30534ddecd0ae769293118703a015d0df3416
madazap <mitchel.dazap@gmail.com>
2023-02-21 21:10:34 -0500

    entrega 3

--- desafio-express/src/app.js
+++ desafio-express/src/app.js
@@ -2,17 +2,38 @@ import express from "express";
 import ProductManager from "./productManager.js";
 
 const producto = new ProductManager();
-let products = [];
 let contador;
 const app=express();
 
 
 app.get("/products", async (req, resp )=>{
     const prods=  await producto.getProducts();
-    resp.send(prods);
+    const {limit}=req.query;
+        if(limit){
+            return resp.send(prods.slice(0,limit));
+        }else{
+            resp.send(prods);
+        }
+    
 });
 
+app.get("/products/:id", async (req, resp) => {
+  const prods = await producto.getProducts();
+  const { id } = req.params;
 
+  if (id) {
+    const producto =prods.find((i)=>i.contador==id);
+        if(producto){
+                return resp.send(producto);
+        }
+            else{
+               resp.send(`El producto con codigo ${id} no existe`); 
+            }
+    
+  } else {
+    resp.send(`El producto no existe`);
+  }
+});
 
 app.listen(8080, ()=> {
     console.log("Server listening on port 8080");
